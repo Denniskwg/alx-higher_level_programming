7092190541e6bc5fc7f2cb9c5c28d3e3a0fb7f3d
Dennis Kamau <dkamau476@gmail.com>
2022-11-28 10:23:09 -0800

    corrections

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,12 +7,22 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *current;
+	listint_t *temp;
 
 	current = list;
-	while (current->next != NULL)
+	while (current != NULL)
 	{
-		if (current->next == list)
+		temp = list->next;
+		while (temp != list && temp != NULL)
+		{
+			if (temp->next == current)
+				return (1);
+			temp = temp->next;
+		}
+		if (temp == list)
 			return (1);
+		if (temp == NULL)
+			return (0);
 		current = current->next;
 	}
 	return (0);
