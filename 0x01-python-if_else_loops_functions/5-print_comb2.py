61331e53fbd3f79b2e42e1cb2b9a057299ad1199
root <dkamau476@gmail.com>
2022-11-29 16:28:11 +0000

    corrections

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i in range(0, 10):
-        print("0{:d}, ".format(i), end="")
-    elif i == 99:
-        print(i)
-    else:
-        print("{:d}, ".format(i), end="")
+print(", ".join("{0:0>2}".format(i) for i in range(100)))
