f488313bf64df3ca3ab756860d206723f5e91413
root <dkamau476@gmail.com>
2022-11-29 16:03:34 +0000

    corrections

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@ for i in range(0, 100):
     if i in range(0, 10):
         print("0{:d}, ".format(i), end="")
     elif i == 99:
-        print("{:d}".format(i))
+        print(i)
     else:
         print("{:d}, ".format(i), end="")
