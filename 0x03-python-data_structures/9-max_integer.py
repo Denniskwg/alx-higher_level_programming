74bc2bdd584162afc5ee69889cf22450213f3d5e
Dennis Kamau <dkamau476@gmail.com>
2022-12-04 03:03:12 -0800

    adding solution for question 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len (my_list)
+    if length == 0:
+        return None
+    else:
+        largest = my_list[0]
+        for i in range(length):
+            if my_list[i] > largest:
+                largest = my_list[i]
+        return largest
