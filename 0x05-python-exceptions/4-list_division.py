e70acfa246190cc27b7ae87fe301022eb2de03f4
Dennis Kamau <dkamau476@gmail.com>
2022-12-19 13:24:30 -0800

    editing solution for question 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     store = []
-    switch = 1
+    switch = 0
     for i in range(list_length):
         try:
             x = my_list_1 / my_list_2
