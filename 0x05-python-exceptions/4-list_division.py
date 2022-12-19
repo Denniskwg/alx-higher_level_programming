098412884860b7da3fc63112e6b810f508b4787f
Dennis Kamau <dkamau476@gmail.com>
2022-12-19 13:19:07 -0800

    adding solution for question 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    store = []
+    switch = 1
+    try:
+        x = my_list_1 / my_list_2
+    except ZeroDivisionError:
+        print("division by 0")
+        switch = 1
+    except TypeError:
+        print("wrong type")
+        switch = 1
+    except IndexError:
+        print("out of Error")
+        switch = 1
+    finally:
+        if switch:
+            switch = 0
+            store.append(0)
+        else:
+            store.append(x)
+    return store
