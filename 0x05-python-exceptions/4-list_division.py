bb5c9d500e992d14244a04f8f738e54aecb467a2
Dennis Kamau <dkamau476@gmail.com>
2022-12-19 13:22:45 -0800

    editing solution for question 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,21 +2,22 @@
 def list_division(my_list_1, my_list_2, list_length):
     store = []
     switch = 1
-    try:
-        x = my_list_1 / my_list_2
-    except ZeroDivisionError:
-        print("division by 0")
-        switch = 1
-    except TypeError:
-        print("wrong type")
-        switch = 1
-    except IndexError:
-        print("out of Error")
-        switch = 1
-    finally:
-        if switch:
-            switch = 0
-            store.append(0)
-        else:
-            store.append(x)
+    for i in range(list_length):
+        try:
+            x = my_list_1 / my_list_2
+        except ZeroDivisionError:
+            print("division by 0")
+            switch = 1
+        except TypeError:
+            print("wrong type")
+            switch = 1
+        except IndexError:
+            print("out of Error")
+            switch = 1
+        finally:
+            if switch:
+                switch = 0
+                store.append(0)
+            else:
+                store.append(x)
     return store
