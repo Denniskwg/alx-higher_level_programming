b9c295c75dbcdd846af2cceb04aefa019a764e7c
root <dkamau476@gmail.com>
2022-12-20 18:39:23 +0000

    adding solution for question 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""1-square
+
+This module defines a class "Square" that defines a square
+"""
+
+
+class Square:
+    """class Square defines a square by size"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not type(self.__size) == int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        Returns:
+            current square area
+        """
+        return self.__size ** 2
