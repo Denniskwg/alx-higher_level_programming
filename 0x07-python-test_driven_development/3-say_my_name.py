1450308c31b9d952022c5f3b9a6398596a8e6583
root <dkamau476@gmail.com>
2023-01-03 14:16:21 +0000

    corrections

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,12 +8,8 @@ a formatted name string
 def say_my_name(first_name, last_name=""):
     """prints My name is <first name> <last name>
     """
-    try:
-        if type(first_name) != str:
-            raise TypeError("first_name must be a string")
-        if type(last_name) != str:
-            raise TypeError("last_name must be a string")
-    except Exception as e:
-        print(e)
-    else:
-        print("My name is {} {}".format(first_name, last_name))
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
