b768661072351afad6414bfb3bbde780434c35ee
root <dkamau476@gmail.com>
2023-01-03 13:04:36 +0000

    editing solution for question 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -28,10 +28,7 @@ class Rectangle:
         else:
             return (self.__width + self.__height) * 2
     def __repr__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        else:
-            return "<3-rectangle.Rectangle object at {}>".format(hex(id(self)))
+        return "<3-rectangle.Rectangle object at {}>".format(hex(id(self)))
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             print("")
