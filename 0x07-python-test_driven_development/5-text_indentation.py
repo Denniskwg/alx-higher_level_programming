813927ebe4cacf149dfb41babc75d3b41d6b9764
root <dkamau476@gmail.com>
2023-01-03 14:31:31 +0000

    corrections

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,17 +9,13 @@ def text_indentation(text):
     """prints text to standard output. if text is
     not a string raise a TypeError exception
     """
-    try:
-        if type(text) != str:
-            raise TypeError("text must be a string")
-    except Exception as e:
-        print(e)
-    else:
-        for index in range(len(text)):
-            if text[index] in (".", "?", ":"):
-                print(text[index], end="")
-                print("\n")
-            elif text[index] == " " and text[index - 1] in (".", "?", ":"):
-                continue
-            else:
-                print(text[index], end="")
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    for index in range(len(text)):
+        if text[index] in (".", "?", ":"):
+            print(text[index], end="")
+            print("\n")
+        elif text[index] == " " and text[index - 1] in (".", "?", ":"):
+            continue
+        else:
+            print(text[index], end="")
