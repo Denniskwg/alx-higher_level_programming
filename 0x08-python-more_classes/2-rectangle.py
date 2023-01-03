cf030faf0eec80f24a746d93df4f8fe5bcbed15a
root <dkamau476@gmail.com>
2023-01-03 18:39:50 +0000

    formatting class

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,6 +3,8 @@
 
 This module defines a class Rectangle that defines a rectangle
 """
+
+
 class Rectangle:
     """Defines a rectangle"""
     def __init__(self, width=0, height=0):
@@ -18,19 +20,23 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = height
+
     def area(self):
         """Returns area of a rectangle"""
         return self.__width * self.__height
+
     def perimeter(self):
         """Returns perimeter of a rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return (self.__width + self.__height) * 2
+
     @property
     def width(self):
         """Returns current width of rectangle"""
         return self.__width
+
     @width.setter
     def width(self, value):
         """
@@ -43,10 +49,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
+
     @property
     def height(self):
         """Returns current height of rectangle"""
         return self.__height
+
     @height.setter
     def height(self, value):
         """
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -3,6 +3,8 @@
 
 This module defines a class Rectangle that defines a rectangle class
 """
+
+
 class Rectangle:
     """Defines a rectangle"""
     def __init__(self, width=0, height=0):
@@ -18,25 +20,30 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = height
+
     def area(self):
         """Returns area of a rectangle"""
         return self.__width * self.__height
+
     def perimeter(self):
         """Returns perimeter of a rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return (self.__width + self.__height) * 2
+
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
         else:
             result = '\n'.join("#" * self.__width for j in range(self.__height))
             return result
+
     @property
     def width(self):
         """Returns current width of rectangle"""
         return self.__width
+
     @width.setter
     def width(self, value):
         """
@@ -49,10 +56,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
+
     @property
     def height(self):
         """Returns current height of rectangle"""
         return self.__height
+
     @height.setter
     def height(self, value):
         """
