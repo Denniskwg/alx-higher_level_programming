0ffb561cbe722a2058a419aabb3a9196897bcd4d
root <dkamau476@gmail.com>
2023-01-03 12:59:59 +0000

    adding solution for question 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""0-rectangle
+
+This module defines a class Rectangle that defines a rectangle
+"""
+class Rectangle:
+    """Defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+    def area(self):
+        """Returns area of a rectangle"""
+        return self.__width * self.__height
+    def perimeter(self):
+        """Returns perimeter of a rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
+    def __repr__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            return "<3-rectangle.Rectangle object at {}>".format(hex(id(self)))
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            print("")
+        else:
+            result = '\n'.join("#" * self.__width for j in range(self.__height))
+            return result
+    @property
+    def width(self):
+        """Returns current width of rectangle"""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """
+        Args:
+            value (int): value to set self.__width
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    @property
+    def height(self):
+        """Returns current height of rectangle"""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """
+        Args:
+            value (int): value to set self.__width
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
