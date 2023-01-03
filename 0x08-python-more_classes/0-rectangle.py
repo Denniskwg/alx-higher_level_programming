715e56efe421424ba31a47e5b5ab9d998b7fec24
root <dkamau476@gmail.com>
2023-01-03 09:58:02 +0000

    adding solution for question 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This is the "0-rectangle" module
+
+"""
+class Rectangle:
+    """Defines a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+# Python - More Classes and Objects
+- More exercises on python classes, objects and oop.
