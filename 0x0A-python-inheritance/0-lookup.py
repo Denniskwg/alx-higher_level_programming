dd8886ef2f0e664f07e78625e7ad3001bb13200d
Dennis Kamau <dkamau476@gmail.com>
2023-01-09 06:06:20 -0800

    initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pyhon3
+
+"""0-lookup
+This is the "0-lookup" module
+
+"""
+
+def lookup(obj):
+    """Returns a list of available attributes and methods
+    of an oject
+    """
+    return dir(obj)
