82e29b4e879cb9bbe1af1eb5347666d5774cde8a
Dennis Kamau <dkamau476@gmail.com>
2023-01-10 04:10:25 -0800

    initial commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""o-read_file module defines a function read_file that 
+reads a text file and prints it to stdout
+
+"""
+
+def read_file(filename=""):
+    """reads and prints the contents of passed file argument"""
+    with open(filename) as f:
+        for line in f:
+            print(line)
