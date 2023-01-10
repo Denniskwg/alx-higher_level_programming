24a15064e4d3e0ac6a52a5ae260b42350dce0ba0
Dennis Kamau <dkamau476@gmail.com>
2023-01-10 04:12:35 -0800

    adding solution for question 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-"""o-read_file module defines a function read_file that 
+"""o-read_file module defines a function read_file that
 reads a text file and prints it to stdout
 
 """
+
 
 def read_file(filename=""):
     """reads and prints the contents of passed file argument"""
