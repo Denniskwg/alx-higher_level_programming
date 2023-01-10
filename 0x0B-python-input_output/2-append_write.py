d96ccd64045784496ad1ca6ac9739992386a37a7
Dennis Kamau <dkamau476@gmail.com>
2023-01-10 04:38:01 -0800

    adding solution for question 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""2-append_write module defines a function append_write that
+appends to a text file and returns the number of characters
+written
+
+"""
+
+
+def append_write(filename="", text=""):
+    """returns the number of characters appended to filename passed"""
+    with open(filename, "a") as f:
+        return (f.write(text))
