cb50c4726922d4e833c2fccdaf7d59820ac9e429
Dennis Kamau <dkamau476@gmail.com>
2023-01-10 14:02:58 -0800

    adding solution for question 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Defines a class Student with method to_json and attributes
+first name, last name and age.
+
+"""
+
+
+class Student:
+    """class that defines a student."""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs and all(type(item) == str for item in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            return (self.__dict__)
