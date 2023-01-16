63df4806b9361cca011035da2e5934d8e54bfcb6
Denniskwg <dkamau476@gmail.com>
2023-01-16 15:15:51 -0800

    corrections

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,7 +22,7 @@ class Base:
         """Returns the JSON string representation of passed
         object
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
