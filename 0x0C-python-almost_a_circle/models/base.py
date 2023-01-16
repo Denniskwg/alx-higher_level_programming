1c82ff2eabbba4962f7899276f3cdc60e614baeb
Denniskwg <dkamau476@gmail.com>
2023-01-16 15:12:01 -0800

    adding solution for question 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@
 attribute __nb_objects
 
 """
+import json
 
 
 class Base:
@@ -16,3 +17,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of passed
+        object
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
