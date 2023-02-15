be3252781ed9f15fdb0d26f0da00c31cbee9d6e0
Dennis Kamau <dkamau476@gmail.com>
2023-02-15 12:05:26 -0800

    adding solution for question 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creates a user user_0d_1 with password user_0d_1_pwd
+-- if user exists script shouldnt fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
