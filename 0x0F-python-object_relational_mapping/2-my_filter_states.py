53f8509adba440e1955aaa63cefe0d5ed6a04597
Dennis Kamau <dkamau476@gmail.com>
2023-03-17 12:18:54 -0700

    adding solution for question 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+2-my_filter_states - lists all states from the database hbtn_0e_0_usa
+with a name starting with N (upper N).
+username, password and database will be passed as arguments to the
+script
+"""
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+    usr = sys.argv[1]
+    passw = sys.argv[2]
+    db = sys.argv[3]
+    name = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", user=usr, passwd=passw, db=db)
+    cur = db.cursor()
+    cur.execute("SELECT \
+        * FROM states WHERE name = '{}' ORDER BY id ASC;".format(name))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
