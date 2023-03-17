ebd1729f62d33c47fcfb45315c0c08108fe37e50
Dennis Kamau <dkamau476@gmail.com>
2023-03-17 07:07:57 -0700

    initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+"""
+0-select_states - lists all states from the database hbtn_0e_0_usa
+username, password and database will be passed as arguments to the
+script
+"""
+usr = sys.argv[1]
+passw = sys.argv[2]
+db = sys.argv[3]
+db = MySQLdb.connect(host="localhost", user=usr, passwd=passw, db=db)
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+cur.close()
+db.close()
