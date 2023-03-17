e6d771f8bbe92684c9ee72327707fe70383afae0
Dennis Kamau <dkamau476@gmail.com>
2023-03-17 08:50:56 -0700

    adding solution for question 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+1-select_states - lists all states from the database hbtn_0e_0_usa
+with a name starting with N (upper N).
+username, password and database will be passed as arguments to the
+script
+"""
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+    usr = sys.argv[1]
+    passw = sys.argv[2]
+    db = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", user=usr, passwd=passw, db=db)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
