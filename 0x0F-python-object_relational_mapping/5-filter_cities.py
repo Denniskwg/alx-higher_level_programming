1814d4134a4b8feec3b55077ff84db7b1793856d
Dennis Kamau <dkamau476@gmail.com>
2023-03-18 02:54:51 -0700

    adding solution for question 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+5-filter_cities - lists all cities
+from the database hbtn_0e_4_usa where state is equal to passed
+argument
+username, password and database will be passed as arguments to the
+script
+"""
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+    usr = sys.argv[1]
+    passw = sys.argv[2]
+    db = sys.argv[3]
+    sql = """SELECT cities.name
+         FROM states
+         INNER JOIN cities
+         ON states.id = cities.state_id
+         WHERE states.name = %s
+         ORDER BY cities.id ASC
+         """
+    db = MySQLdb.connect(host="localhost", user=usr, passwd=passw, db=db)
+    cur = db.cursor()
+    cur.execute(sql, (sys.argv[4],))
+    rows = cur.fetchall()
+    for i in range(len(rows)):
+        if i < len(rows) - 1:
+            print("{}, ".format(rows[i][0]), end="")
+        else:
+            print(rows[i][0])
+    cur.close()
+    db.close()
