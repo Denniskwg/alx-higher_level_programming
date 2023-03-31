241e4c261fb9320658578c6f4a8f76b5b79494d2
Denniskwg <dkamau476@gmail.com>
2023-03-31 05:04:27 -0700

    adding solution for question 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""6-peak defines a function find peak that finds
+a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    finds the peak of a list of unsorted integers
+    Args:
+        list_of_integers -  a list of unsorted integers
+    returns the peak point of a list
+    """
+    sums = 0
+    drop = 0
+    i = 0
+    index = 0
+    flag = 0
+    if len(list_of_integers) == 0:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    while i < len(list_of_integers) - 1:
+        if list_of_integers[i] >= list_of_integers[i + 1]:
+            if drop < list_of_integers[i]:
+                drop = list_of_integers[i]
+            if list_of_integers[i] == list_of_integers[i + 1]:
+                flag = 1
+                return drop
+        else:
+            addition = list_of_integers[i] + list_of_integers[i + 1]
+            if flag == 1:
+                index = list_of_integers.index(drop)
+            else:
+                index = i + 1
+            if addition > sums:
+                sums = addition
+        i = i + 1
+    return list_of_integers[index]
