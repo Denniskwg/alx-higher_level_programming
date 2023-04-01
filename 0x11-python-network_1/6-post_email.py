61a8f374ff89b52f940564127c7c7d926cca85a7
Denniskwg <dkamau476@gmail.com>
2023-04-01 13:59:09 +0300

    corrections

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,5 +10,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     req = requests.post(url, data={'email': email})
-    res = requests.get(url)
-    print(res.text)
+    print(req.text)
