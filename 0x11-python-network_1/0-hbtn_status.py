6a76d51e4c5f59fac6eb2e17696724916074bc0c
Denniskwg <dkamau476@gmail.com>
2023-04-01 10:53:58 +0300

    initial commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""0-hbtn_status fetches https://alx-intranet.hbtn.io/status
+and prints the body content
+"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        response = response.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(response)))
+        print("\t - content: {}".format(response))
+        print("\t - utf8 content: {}".format(response.decode('utf-8')))
