46ab8dd8f68a84954cbfab792d231b8be45b4eb6
Denniskwg <dkamau476@gmail.com>
2023-04-01 12:56:03 +0300

    adding solution for question 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""0-hbtn_status fetches https://alx-intranet.hbtn.io/status
+and prints the body content
+"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    import urllib.parse
+    import urllib.error
+    url = sys.argv[1]
+    try:
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
+            response = response.read()
+    except urllib.error.HTTPError as e:
+        print(e.code)
+    else:
+        print(response.decode('utf-8'))
