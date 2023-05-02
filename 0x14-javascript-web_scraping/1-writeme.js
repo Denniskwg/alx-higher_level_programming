0468e700e7c7bd07c40b1b1561511120c6b3f575
Denniskwg <dkamau476@gmail.com>
2023-05-02 13:29:18 +0300

    adding solution for number 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+const str = process.argv[3];
+fs.appendFile(file, str, function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
